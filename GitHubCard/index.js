6f839e175ddf41f94cacd135d54e444604d7b9fc
mmitch2958 <mikescottmitchell@outlook.com>
2020-08-06 16:18:56 -0400

    finished index.js

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,26 @@
+// import axios from axios
+
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/mmitch2958').then(response => {
+  const cards = document.querySelector('.cards')
+  cards.append(cardMaker(response.data))
+
+
+axios.get(response.data.followers_url).then(res => {
+  console.log(res.data)
+  res.data.forEach(elem => {
+    axios.get('https://api.github.com/users/' + elem.login).then(res => {
+      cards.append(cardMaker(res.data))
+    })
+  })
+})
+})
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,29 +46,83 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+let cardMaker = (data) => {
+  //Create copy of data to go in object 
+  const card = document.createElement('div');
+  const image = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const username = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const profileLink = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+
+  //constrcut card 
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  name.classList.add('name')
+  username.classList.add('username')
+
+  image.src = data.avatar_url
+  name.textContent = data.name
+  username.textContent = data.login
+  location.textContent = data.location
+  profileLink.href = data.html_url
+  profileLink.textContent = data.html_url
+  profile.textContent = `Profile: `
+  followers.textContent = `Followers: ${data.followers}`
+  following.textContent = `Following: ${data.following}`
+  bio.textContent = `Bio: ${data.bio}`
+
+  //Append Information 
+  cardInfo.appendChild(name)
+  cardInfo.appendChild(username)
+  cardInfo.appendChild(location)
+  profile.appendChild(profileLink)
+  cardInfo.appendChild(profile)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)
+  
+  card.appendChild(image)
+  card.appendChild(cardInfo)
+
+  //Return Object 
+
+  return card
+}
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
-
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
-
 /*
+
+function cardM(data){
+  const component = `
+  
+  <div class="card">
+  <img src=${data.img} />
+  <div class="card-info">
+    <h3 class="name">{users name}</h3>
+    <p class="username">{users user name}</p>
+    <p>Location: {users location}</p>
+    <p>Profile:  
+      <a href={address to users github page}>{address to users github page}</a>
+    </p>
+    <p>Followers: {users followers count}</p>
+    <p>Following: {users following count}</p>
+    <p>Bio: {users bio}</p>
+  </div>
+</div>
+  `
+  return component
+}git 
   List of LS Instructors Github username's:
     tetondan
     dustinmyers
